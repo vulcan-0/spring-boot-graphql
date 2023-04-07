c4cbf2da92dddfabba29c6a653e8de4633d6d52a
Sam Lu <luxiaocong524@163.com>
2023-04-07 08:58:16 +0800

    feat: 提交 chapter5 样例代码

---
+++ chapter5/src/main/java/org/vc121/light/graphql/chapter5/Chapter5GraphqlServerApplication.java
@@ -0,0 +1,17 @@
+package org.vc121.light.graphql.chapter5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lxc
+ * @date 2023/04/06
+ */
+@SpringBootApplication
+public class Chapter5GraphqlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter5GraphqlServerApplication.class);
+    }
+
+}
