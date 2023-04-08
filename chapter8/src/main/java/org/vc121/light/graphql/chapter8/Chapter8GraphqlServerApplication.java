63b15c2d88629dbb4be358f1ef9a569ce6646dbf
Sam Lu <luxiaocong524@163.com>
2023-04-08 12:03:00 +0800

    feat: 提交 chapter8 样例代码

---
+++ chapter8/src/main/java/org/vc121/light/graphql/chapter8/Chapter8GraphqlServerApplication.java
@@ -0,0 +1,17 @@
+package org.vc121.light.graphql.chapter8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lxc
+ * @date 2023/04/06
+ */
+@SpringBootApplication
+public class Chapter8GraphqlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter8GraphqlServerApplication.class);
+    }
+
+}
---
+++ chapter8/src/main/java/org/vc121/light/graphql/chapter8/domain/bank/Client.java
@@ -0,0 +1,23 @@
+package org.vc121.light.graphql.chapter8.domain.bank;
+
+import lombok.Builder;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author lxc
+ * @date 2023/04/06
+ */
+@Setter
+@Builder
+public class Client {
+
+    UUID id;
+    String firstName;
+    List<String> middleNames;
+    String lastName;
+    Client client;
+
+}
