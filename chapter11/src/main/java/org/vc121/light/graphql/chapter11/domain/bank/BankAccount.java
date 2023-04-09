b61688217a40dd86b4f156a09eec261c9dff736d
Sam Lu <luxiaocong524@163.com>
2023-04-09 22:42:02 +0800

    feat: 提交 chapter11 样例代码

---
+++ chapter11/src/main/java/org/vc121/light/graphql/chapter11/domain/bank/BankAccount.java
@@ -0,0 +1,22 @@
+package org.vc121.light.graphql.chapter11.domain.bank;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author lxc
+ * @date 2023/04/05
+ */
+@Builder
+@Value
+public class BankAccount {
+
+    UUID id;
+    Client client;
+    List<Asset> assets;
+    Currency currency;
+
+}
---
+++ chapter11/src/main/java/org/vc121/light/graphql/chapter11/exceptions/GraphqlExceptionHandler.java
@@ -0,0 +1,25 @@
+package org.vc121.light.graphql.chapter11.exceptions;
+
+import graphql.GraphQLException;
+import graphql.kickstart.spring.error.ThrowableGraphQLError;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author lxc
+ * @date 2023/04/08
+ */
+@Component
+public class GraphqlExceptionHandler {
+
+    @ExceptionHandler(GraphQLException.class)
+    public ThrowableGraphQLError handle(GraphQLException e) {
+        return new ThrowableGraphQLError(e);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ThrowableGraphQLError handle(RuntimeException e) {
+        return new ThrowableGraphQLError(e, "Internal Server Error");
+    }
+
+}
