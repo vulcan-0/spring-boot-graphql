f9f632c0ce87b4337dee1e4d912cbcfce5493a50
Sam Lu <luxiaocong524@163.com>
2023-04-10 23:05:45 +0800

    feat: 提交 chapter12 样例代码

---
+++ chapter12/src/main/java/org/vc121/light/graphql/chapter12/Chapter12GraphqlServerApplication.java
@@ -0,0 +1,17 @@
+package org.vc121.light.graphql.chapter12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lxc
+ * @date 2023/04/06
+ */
+@SpringBootApplication
+public class Chapter12GraphqlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter12GraphqlServerApplication.class);
+    }
+
+}
