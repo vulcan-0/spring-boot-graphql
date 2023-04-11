cfbe81484bc3761011fd460061cd1a251840aae8
Sam Lu <luxiaocong524@163.com>
2023-04-11 20:58:45 +0800

    feat: 提交 chapter14 样例代码

---
+++ chapter14/src/main/java/org/vc121/light/graphql/chapter14/domain/bank/Asset.java
@@ -0,0 +1,16 @@
+package org.vc121.light.graphql.chapter14.domain.bank;
+
+import lombok.Builder;
+
+import java.util.UUID;
+
+/**
+ * @author lxc
+ * @date 2023/04/09
+ */
+@Builder
+public class Asset {
+
+    UUID id;
+
+}
