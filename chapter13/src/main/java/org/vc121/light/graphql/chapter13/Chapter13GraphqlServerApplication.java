1b548b7f057fd1181cae79315ee830f515ee248f
Sam Lu <luxiaocong524@163.com>
2023-04-11 20:31:34 +0800

    feat: 提交 chapter13 样例代码

---
+++ chapter13/src/main/java/org/vc121/light/graphql/chapter13/Chapter13GraphqlServerApplication.java
@@ -0,0 +1,17 @@
+package org.vc121.light.graphql.chapter13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lxc
+ * @date 2023/04/06
+ */
+@SpringBootApplication
+public class Chapter13GraphqlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter13GraphqlServerApplication.class);
+    }
+
+}
---
+++ chapter13/src/main/java/org/vc121/light/graphql/chapter13/domain/bank/input/CreateBankAccountInput.java
@@ -0,0 +1,14 @@
+package org.vc121.light.graphql.chapter13.domain.bank.input;
+
+import lombok.Data;
+
+/**
+ * @author lxc
+ * @date 2023/04/10
+ */
+@Data
+public class CreateBankAccountInput {
+
+    String firstName;
+
+}
