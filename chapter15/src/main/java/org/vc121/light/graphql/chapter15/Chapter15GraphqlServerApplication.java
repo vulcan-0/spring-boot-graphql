50bda8d5daef53d99e391459e2858afbd6bd3c4a
Sam Lu <luxiaocong524@163.com>
2023-04-12 08:46:25 +0800

    feat: 提交 chapter15 样例代码

---
+++ chapter15/src/main/java/org/vc121/light/graphql/chapter15/Chapter15GraphqlServerApplication.java
@@ -0,0 +1,17 @@
+package org.vc121.light.graphql.chapter15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lxc
+ * @date 2023/04/06
+ */
+@SpringBootApplication
+public class Chapter15GraphqlServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter15GraphqlServerApplication.class);
+    }
+
+}
---
+++ chapter15/src/main/java/org/vc121/light/graphql/chapter15/resolver/bank/mutation/BankAccountMutation.java
@@ -0,0 +1,33 @@
+package org.vc121.light.graphql.chapter15.resolver.bank.mutation;
+
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.vc121.light.graphql.chapter15.domain.bank.BankAccount;
+import org.vc121.light.graphql.chapter15.domain.bank.Client;
+import org.vc121.light.graphql.chapter15.domain.bank.Currency;
+import org.vc121.light.graphql.chapter15.domain.bank.input.CreateBankAccountInput;
+
+import java.util.UUID;
+
+/**
+ * @author lxc
+ * @date 2023/04/10
+ */
+@Slf4j
+@Component
+public class BankAccountMutation implements GraphQLMutationResolver {
+
+    public BankAccount createBankAccount(CreateBankAccountInput input, DataFetchingEnvironment e) {
+        log.info("Creating bank account for {}", input);
+
+        return BankAccount.builder().id(UUID.randomUUID())
+                .client(Client.builder().id(UUID.randomUUID())
+                        .firstName(input.getFirstName())
+                        .build())
+                .currency(Currency.USD)
+                .build();
+    }
+
+}
