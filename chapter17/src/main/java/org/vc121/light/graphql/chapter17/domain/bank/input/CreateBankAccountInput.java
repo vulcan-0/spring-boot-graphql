4dc028d42e211819a15cb190b973be62bc55555e
Sam Lu <luxiaocong524@163.com>
2023-04-14 08:50:47 +0800

    feat: 提交 chapter17 样例代码

---
+++ chapter17/src/main/java/org/vc121/light/graphql/chapter17/domain/bank/input/CreateBankAccountInput.java
@@ -0,0 +1,15 @@
+package org.vc121.light.graphql.chapter17.domain.bank.input;
+
+import lombok.Data;
+
+/**
+ * @author lxc
+ * @date 2023/04/10
+ */
+@Data
+public class CreateBankAccountInput {
+
+    String firstName;
+    int age;
+
+}
---
+++ chapter17/src/main/java/org/vc121/light/graphql/chapter17/resolver/bank/mutation/UploadFileMutation.java
@@ -0,0 +1,30 @@
+package org.vc121.light.graphql.chapter17.resolver.bank.mutation;
+
+import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * @author lxc
+ * @date 2023/04/11
+ */
+@Slf4j
+@Component
+public class UploadFileMutation implements GraphQLMutationResolver {
+
+    public UUID uploadFile(DataFetchingEnvironment environment) {
+        log.info("Uploading file");
+
+        DefaultGraphQLServletContext context = environment.getContext();
+        context.getFileParts().forEach(part -> {
+            log.info("uploading {}, size: {}", part.getSubmittedFileName(), part.getSize());
+        });
+
+        return UUID.randomUUID();
+    }
+
+}
