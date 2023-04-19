512bd1ea2a64984486eeea924d2b3d2c8456c680
Sam Lu <luxiaocong524@163.com>
2023-04-19 08:56:42 +0800

    feat: 提交 chapter22 样例代码

---
+++ chapter22/src/main/java/org/vc121/light/graphql/chapter22/domain/bank/BankAccount.java
@@ -0,0 +1,26 @@
+package org.vc121.light.graphql.chapter22.domain.bank;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author lxc
+ * @date 2023/04/05
+ */
+@Builder
+@Value
+public class BankAccount {
+
+    UUID id;
+    Client client;
+    Currency currency;
+    List<Asset> assets;
+    LocalDate createOn;
+    ZonedDateTime createAt;
+
+}
