b744f42470cbcd267f00878c910a6f34fc5d2496
Sam Lu <luxiaocong524@163.com>
2023-04-22 17:51:04 +0800

    feat: 提交 chapter24 样例代码

---
+++ chapter24/src/main/java/org/vc121/light/graphql/chapter24/connection/CursorUtil.java
@@ -0,0 +1,41 @@
+package org.vc121.light.graphql.chapter24.connection;
+
+import graphql.relay.ConnectionCursor;
+import graphql.relay.DefaultConnectionCursor;
+import graphql.relay.Edge;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author lxc
+ * @date 2023/04/18
+ */
+@Component
+public class CursorUtil {
+
+    @SneakyThrows
+    public ConnectionCursor createCursorWith(UUID id) {
+        return new DefaultConnectionCursor(
+                Base64.getEncoder().encodeToString(id.toString().getBytes(StandardCharsets.UTF_8))
+        );
+    }
+
+    @SneakyThrows
+    public UUID decode(String id) {
+        return UUID.fromString(new String(Base64.getDecoder().decode(id)));
+    }
+
+    public <T> ConnectionCursor getFirstCursorFrom(List<Edge<T>> edges) {
+        return edges.isEmpty() ? null : edges.get(0).getCursor();
+    }
+
+    public <T> ConnectionCursor getLastCursorFrom(List<Edge<T>> edges) {
+        return edges.isEmpty() ? null : edges.get(edges.size() - 1).getCursor();
+    }
+
+}
---
+++ chapter24/src/main/java/org/vc121/light/graphql/chapter24/context/CustomGraphQLContextBuilder.java
@@ -0,0 +1,48 @@
+package org.vc121.light.graphql.chapter24.context;
+
+import graphql.kickstart.execution.context.GraphQLContext;
+import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
+import graphql.kickstart.servlet.context.GraphQLServletContextBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.vc121.light.graphql.chapter24.dataloader.DataLoaderRegistryFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.Session;
+import javax.websocket.server.HandshakeRequest;
+
+/**
+ * @author lxc
+ * @date 2023/04/19
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomGraphQLContextBuilder implements GraphQLServletContextBuilder {
+
+    private final DataLoaderRegistryFactory dataLoaderRegistryFactory;
+
+    @Override
+    public GraphQLContext build(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        var userId = httpServletRequest.getHeader("user_id");
+        var context = DefaultGraphQLServletContext.createServletContext()
+                .with(httpServletRequest)
+                .with(httpServletResponse)
+                .with(dataLoaderRegistryFactory.create(userId))
+                .build();
+        return new CustomGraphQLContext(userId, context);
+    }
+
+    @Override
+    public GraphQLContext build(Session session, HandshakeRequest handshakeRequest) {
+        throw new IllegalStateException("Unsupported");
+    }
+
+    @Override
+    public GraphQLContext build() {
+        throw new IllegalStateException("Unsupported");
+    }
+
+}
