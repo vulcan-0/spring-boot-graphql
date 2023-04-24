369ed2bb42cf806b4cc4f4d573ea4692ee3dada7
Sam Lu <luxiaocong524@163.com>
2023-04-24 13:22:33 +0800

    feat: 提交 chapter27 样例代码

---
+++ chapter27/src/main/java/org/vc121/light/graphql/chapter27/BankAccountRepository.java
@@ -0,0 +1,64 @@
+package org.vc121.light.graphql.chapter27;
+
+import org.springframework.stereotype.Component;
+import org.vc121.light.graphql.chapter27.domain.bank.BankAccount;
+import org.vc121.light.graphql.chapter27.domain.bank.Currency;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * @author lxc
+ * @date 2023/04/18
+ */
+@Component
+public class BankAccountRepository {
+
+    private final List<BankAccount> bankAccounts = List.of(
+            BankAccount.builder()
+                    .id(UUID.fromString("5b6d95d9-9404-4c1a-964f-a7b711ae6660"))
+                    .currency(Currency.USD)
+                    .createAt(ZonedDateTime.parse("2023-04-18T08:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("5b6d95d9-9404-4c1a-964f-a7b711ae6661"))
+                    .currency(Currency.CHF)
+                    .createAt(ZonedDateTime.parse("2023-04-18T08:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("5b6d95d9-9404-4c1a-964f-a7b711ae6662"))
+                    .currency(Currency.CHF)
+                    .createAt(ZonedDateTime.parse("2023-04-18T08:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("5b6d95d9-9404-4c1a-964f-a7b711ae6663"))
+                    .currency(Currency.CHF)
+                    .createAt(ZonedDateTime.parse("2023-04-18T08:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("5b6d95d9-9404-4c1a-964f-a7b711ae6664"))
+                    .currency(Currency.USD)
+                    .createAt(ZonedDateTime.parse("2023-04-18T08:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("5b6d95d9-9404-4c1a-964f-a7b711ae6665"))
+                    .currency(Currency.USD)
+                    .createAt(ZonedDateTime.parse("2023-04-18T08:30:00+00:00"))
+                    .build()
+    );
+
+    public List<BankAccount> getBankAccounts() {
+        return bankAccounts;
+    }
+
+    public List<BankAccount> getBankAccountsAfter(UUID id) {
+        return bankAccounts.stream()
+                .dropWhile(bankAccount -> {
+                    return bankAccount.getId().compareTo(id) < 0;
+                })
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+}
